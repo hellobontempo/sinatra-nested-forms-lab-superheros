3e3a55455e03a8887aa5a4085902be9529271ac9
claire <clairebontempo@gmail.com>
2020-12-02 14:13:37 -0800

    commit tree d8038bcded9bde653b6f3a518ebb5fbcf32491f0

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,5 +4,20 @@ class App < Sinatra::Base
 
     set :views, Proc.new { File.join(root, "../views/") }
 
+    get '/' do
+        erb :form
+    end
+
+    post '/' do
+        @team = Team.new(params)
+        params[:team][:members].each do |details|
+            Hero.new(details)
+        end
+        @heroes = Hero.all
+        erb :team 
+    end
+
+
+
 
 end
